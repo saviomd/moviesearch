c3829c087337cbe6d8431a231b274c0766a91fb9
Sávio Mendes <saviomd@gmail.com>
2018-06-07 00:04:45 -0300

    Add page not found component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import PageHome from '@/components/PageHome';
+import PageNotFound from '@/components/PageNotFound';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'PageHome',
       component: PageHome,
+    },
+    {
+      path: '*',
+      name: 'PageNotFound',
+      component: PageNotFound,
     },
   ],
 });
