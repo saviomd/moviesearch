de853c2cc641ba9b5c360f04c6667c04406d937a
Sávio Mendes <saviomd@gmail.com>
2018-06-07 17:18:44 -0300

    Add srcset to responsive image component

--- src/components/ResponsiveImage.vue
+++ src/components/ResponsiveImage.vue
@@ -2,6 +2,7 @@
   img.bg-secondary.w-100(
     :alt='this.alt',
     :src='this.src',
+    :srcset='this.srcset',
   )
 </template>
 
@@ -16,6 +17,19 @@ export default {
       const images = store.state.configuration.images;
       const size = (this.type === 'person' ? images.profile_sizes[0] : images.poster_sizes[0]);
       return `${images.secure_base_url}${size}${this.path}`;
+    },
+    srcset() {
+      const images = store.state.configuration.images;
+      const sizes = (this.type === 'person' ? images.profile_sizes : images.poster_sizes);
+      const srcset = [];
+      sizes.forEach((size) => {
+        if (size !== 'original') {
+          const srcsetUrl = `${images.secure_base_url}${size}${this.path}`;
+          const srcsetSize = `${size.split('w')[1]}w`;
+          srcset.push(`${srcsetUrl} ${srcsetSize}`);
+        }
+      });
+      return srcset.toString();
     },
   },
 };
