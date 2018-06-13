410125532f7d67409a1e032b945cf0d088008a7c
Sávio Mendes <saviomd@gmail.com>
2018-06-13 20:33:40 -0300

    Update responsive image component with fallbacks

--- src/components/ResponsiveImage.vue
+++ src/components/ResponsiveImage.vue
@@ -15,21 +15,27 @@ export default {
   computed: {
     src() {
       const images = store.state.configuration.images;
-      const size = (this.type === 'person' ? images.profile_sizes[0] : images.poster_sizes[0]);
-      return `${images.secure_base_url}${size}${this.path}`;
+      if (this.path && images.profile_sizes && images.poster_sizes) {
+        const size = (this.type === 'person' ? images.profile_sizes[0] : images.poster_sizes[0]);
+        return `${images.secure_base_url}${size}${this.path}`;
+      }
+      return null;
     },
     srcset() {
       const images = store.state.configuration.images;
       const sizes = (this.type === 'person' ? images.profile_sizes : images.poster_sizes);
-      const srcset = [];
-      sizes.forEach((size) => {
-        if (size !== 'original') {
-          const srcsetUrl = `${images.secure_base_url}${size}${this.path}`;
-          const srcsetSize = `${size.split('w')[1]}w`;
-          srcset.push(`${srcsetUrl} ${srcsetSize}`);
-        }
-      });
-      return srcset.toString();
+      if (this.path && sizes) {
+        const srcset = [];
+        sizes.forEach((size) => {
+          if (size !== 'original') {
+            const srcsetUrl = `${images.secure_base_url}${size}${this.path}`;
+            const srcsetSize = `${size.split('w')[1]}w`;
+            srcset.push(`${srcsetUrl} ${srcsetSize}`);
+          }
+        });
+        return srcset.toString();
+      }
+      return null;
     },
   },
 };
